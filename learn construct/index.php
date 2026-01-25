33b47b6528f1ad347233ac547dc8909225118c19
GoopsSoup <thoughblud@gmail.com>
2026-01-25 19:14:56 +0700

    Improved code

--- learn construct/index.php
+++ learn construct/index.php
@@ -8,18 +8,34 @@
             $this->health = $health; 
         }
 
-        public function damageTaken(int $damage):void {
-            $this->health -= $damage;
+        public function damageTaken(int $damage):int {
+            return $this->health -= $damage;
+        }
+
+        public function healthHealed(int $heal) {
+            if ($this->health += $heal > 100) {
+                return $this->health = 100;
+            }
+            return $this->health += $heal;
         }
 
         public function showStatus():string {
-            return "Name: $this->name \nHealth: $this->health\n";
+            if ($this->health <= 0) {
+                return "Player died\n";
+            }
+            return "Name: $this->name \nHealth: $this->health\n\n";
         }
     }
 
     $player = new Status("Player-1", 100);
     echo $player->showStatus();
-    
-    echo $player->damageTaken(29);
+
+    $player->damageTaken(67);
+    echo $player->showStatus();
+
+    $player->healthHealed(100);
+    echo $player->showStatus();
+
+    $player->damageTaken(67);
     echo $player->showStatus();
 ?>
