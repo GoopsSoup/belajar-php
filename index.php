251aed10934991a330ea5fb6fdbb172a5cc5717d
GoopsSoup <thoughblud@gmail.com>
2026-02-03 19:08:38 +0700

    new addition

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
-    echo "Hello World!"
+class Store {
+    private $items = [];
+    private int $price;
+    private string $customer;
+
+    public function __construct(string $customer) {
+        $this->customer = $customer;
+    }
+
+    public function addItem($item): string {
+        return $this->items[] = $item;
+    }
+    
+    public function getItems() {
+        foreach ($this->items as $things) {
+            return $things;
+        }
+    }
+}
+
+class Customer extends Store{
+    
+    public function __construct(string $customer) {
+        parent::__construct($customer);
+    }
+}
+
+$bought = new Store("Andhika");
+$bought->addItem('Laptop');
+echo $bought->getItems();
+
 ?>
